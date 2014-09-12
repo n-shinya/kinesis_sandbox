d67b0e2f878714389bfcef6d5b14fa45a6de2076
Shinya Nishinaka <nishinaka@waku-2.com>
2014-09-12 21:55:08 +0900

    Add KCL.

---
+++ kcl/src/main/java/samples/KinesisMessageModel.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Amazon Software License (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/asl/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package samples;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 
+ * This is the data model for the objects being sent through the Amazon Kinesis streams in the samples
+ * 
+ */
+public class KinesisMessageModel implements Serializable {
+
+    /**
+     * Default constructor for Jackson JSON mapper - uses bean pattern.
+     */
+    public KinesisMessageModel() {
+    }
+
+    protected String id;
+    protected String createdAt;
+    protected String text;
+    protected String friendsCount;
+    protected String followersCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getFriendsCount() {
+        return friendsCount;
+    }
+
+    public void setFriendsCount(String friendsCount) {
+        this.friendsCount = friendsCount;
+    }
+
+    public String getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(String followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return super.toString();
+        }
+    }
+
+}
